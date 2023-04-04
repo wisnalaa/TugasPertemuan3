4a048a9a3558649d8fb3703e4727ae6234f099d5
wisnalaa <wisnala34@gmail.com>
2023-04-04 14:42:49 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan3 
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPasangger = 10;
+
+            taxi.taxiInfo();
+            taxi.PickUpPasangger();
+            taxi.DropOffPasangger();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan3
+{
+    class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPasangger { get; set; }
+
+        public void taxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            if (OnDuty)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Pasangger : {0}", NumPasangger);
+        }
+
+        public void PickUpPasangger()
+        {
+            Console.WriteLine("\n{0} sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPasangger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
+    }
+
+}
